70f9e832e8ba2c73beb483720b28f308df665204
Valera Melnikov <melnikov.vv@greendatasoft.ru>
2023-02-23 13:16:19 +0300

    use css vars

--- src/App.js
+++ src/App.js
@@ -2,32 +2,20 @@ import React, { useState } from 'react';
 import './App.css';
 import Button from './button';
 
-
 function App() {
-
-  const [buttonCount, setButtonCount] = useState(5);
-  const [padding, setPadding] = useState("0.5rem");
-  const [borderWidth, setBorderWidth] = useState("1px");
-  const [buttonColor, setButtonColor] = useState("var(--ads-color-orange-500)");
-  const [fontSize, setFontSize] = useState("1rem");
-  const [margin, setMargin] = useState("2px");
-  const [color, setColor] = useState("var(--ads-color-black-0)");
+  const [buttonCount, setButtonCount] = useState(10000);
+  const root = document.querySelector(':root');
+  const setPadding = (val) => root.style.setProperty('--button-padding', val);
+  const setBorderWidth = (val) => root.style.setProperty('--button-border-width', val);
+  const setButtonColor = (val) => root.style.setProperty('--button-bg', val);
+  const setFontSize = (val) => root.style.setProperty('--button-font-size', val);
+  const setMargin = (val) => root.style.setProperty('--button-margin', val);
+  const setColor = (val) => root.style.setProperty('--button-color', val);
 
   const buttons = [];
   for(let i = 0; i < buttonCount; i++) {
     buttons.push(
-      <Button
-        style={{
-          padding,
-          borderWidth: borderWidth,
-          borderColor: buttonColor,
-          fontSize,
-          margin,
-          backgroundColor: buttonColor,
-          color,
-        }}
-        key={i.toString()}
-      >
+      <Button key={i.toString()}>
         Button
       </Button>
     );
@@ -36,7 +24,7 @@ function App() {
   return (
     <div className="main">
       <div className="left-section">
-        <h1>Buttons</h1>
+        <h1>CSS vars buttons</h1>
         <div className='btn-container'>
           {buttons}
         </div>
@@ -54,50 +42,61 @@ function App() {
         <br/>
          <button onClick={() => setButtonCount(buttonCount + 1000)}>Render 1000 buttons</button>
         <br/>
+
         <label>Padding</label>
-        <select value={padding} onChange={(e) => setPadding(e.target.value)}>
+        <select onChange={(e) => setPadding(e.target.value)}>
           <option value="0">Padding</option>
           <option value="0.5rem">0.5rem</option>
           <option value="1rem">1rem</option>
           <option value="1.5rem">1.5rem</option>
           <option value="2rem">2rem</option>
         </select>
+
         <br/>
+
         <label>Border Width</label>
-        <select value={borderWidth} onChange={(e) => setBorderWidth(e.target.value)}>
+        <select onChange={(e) => setBorderWidth(e.target.value)}>
           <option value="0">Border Width</option>
           <option value="1px">1 px</option>
           <option value="2px">2 px</option>
           <option value="3px">3 px</option>
         </select>
+
         <br/>
+
         <label>Button Color</label>
-        <select value={buttonColor} onChange={(e) => setButtonColor(e.target.value)}>
+        <select onChange={(e) => setButtonColor(e.target.value)}>
           <option value="white">Button Color</option>
           <option value="var(--ads-color-orange-500)">Orange</option>
           <option value="blue">Blue</option>
           <option value="green">Green</option>
           <option value="red">Red</option>
         </select>
+
         <br/>
+
         <label>Font Size</label>
-        <select value={fontSize} onChange={(e) => setFontSize(e.target.value)}>
+        <select onChange={(e) => setFontSize(e.target.value)}>
           <option value="0">Font Size</option>
           <option value="1rem">1 rem</option>
           <option value="1.5rem">1.5 rem</option>
           <option value="2rem">2 rem</option>
         </select>
+
         <br/>
+
         <label>Margin</label>
-        <select value={margin} onChange={(e) => setMargin(e.target.value)}>
+        <select onChange={(e) => setMargin(e.target.value)}>
           <option value="0">Margin</option>
           <option value="2px">2 px</option>
           <option value="4px">4 px</option>
           <option value="6px">6 px</option>
         </select>
+
         <br/>
+
         <label>Color</label>
-        <select value={color} onChange={(e) => setColor(e.target.value)}>
+        <select onChange={(e) => setColor(e.target.value)}>
           <option value="white">Color</option>
           <option value="var(--ads-color-black)">Black</option>
           <option value="white">White</option>
